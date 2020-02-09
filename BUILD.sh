a5161fc6275c3e47e7b331b345121379c3a5ab6b
Weslen Nascimento <weslen-7316@outlook.com>
2020-02-08 21:42:07 -0300

    ride service created

--- BUILD.sh
+++ BUILD.sh
@@ -1,1 +1,4 @@
 protoc protos/*.proto --go_out=plugins=grpc:.
+
+yarn install
+protoc protos/*.proto --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" --js_out="import_style=commonjs,binary:." --ts_out="."
