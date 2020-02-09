c9f366c4bf0f031a306bbf5f5494fe23ac0e9537
Weslen Nascimento <weslen-7316@outlook.com>
2020-02-08 22:24:41 -0300

    first request with node

--- BUILD.sh
+++ BUILD.sh
@@ -1,4 +1,9 @@
 protoc protos/*.proto --go_out=plugins=grpc:.
 
 yarn install
-protoc protos/*.proto --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" --js_out="import_style=commonjs,binary:." --ts_out="."
+protoc protos/*.proto \
+  --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts" \
+  --plugin="protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin" \
+  --js_out="import_style=commonjs,binary:." \
+  --ts_out="service=grpc-node:." \
+  --grpc_out="."
--- services/ride/src/index.ts
+++ services/ride/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Initial code :D");
+import grpc from "grpc";
+import messages from "../../../protos/pricing_pb";
+import services from "../../../protos/pricing_grpc_pb";
+
+Promise.resolve().then(() => {
+  const client = new services.PricingClient(
+    "pricing-api:50051",
+    grpc.credentials.createInsecure()
+  );
+
+  const request = new messages.GetPricingFeesByCityRequest();
+  request.setCity("SAO_PAULO");
+
+  client.getPricingFeesByCity(request, (err, res) => {
+    console.log("Response from Golang: ", res?.toObject());
+  });
+});
